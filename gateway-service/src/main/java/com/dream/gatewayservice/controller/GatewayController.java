b12bdcd856c0f80ef2a1c8fbfbab78358f52a1ff
phm940916 <phm940916@gmail.com>
2022-03-30 17:13:45 +0900

    Merge branch 'master' of https://github.com/Advanc2d/MSA__Project
    # Conflicts:
    #	gateway-service/src/main/resources/application.yml

--- gateway-service/src/main/java/com/dream/gatewayservice/controller/GatewayController.java
+++ gateway-service/src/main/java/com/dream/gatewayservice/controller/GatewayController.java
@@ -1,16 +1,28 @@
 package com.dream.gatewayservice.controller;
 
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class GatewayController {
 
    @GetMapping("/")
    public String index(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
       return authorizedClient.getAccessToken().getTokenValue();
+   }
+   
+   @RequestMapping("/fallback/Faliure")
+   public ServerHttpResponse testFallback(ServerHttpRequest request, ServerHttpResponse response) {
+	   log.info("fallback ...............");
+       return response;
    }
 
 }
